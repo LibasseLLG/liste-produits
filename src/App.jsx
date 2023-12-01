aa56d91b3e732da7f724f05519e7b60169b6e027
Libasse <libasse.g.laye@gmail.com>
2023-12-01 22:16:04 +0100

    Premier commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import {Input} from "./components/forms/input.jsx"
+import { Checkbox } from "./components/forms/Checkbox.jsx"
+import { ProductCategoryRow } from "./components/products/productCategoryRow.jsx"
+import { ProductRow } from "./components/products/productRow.jsx"
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinash"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+]
+
+function App () {
+  return <div className="container my-3">
+      <SearchBar />
+      <ProductTable products={PRODUCTS}/>
+  </div>
+}
+
+function SearchBar () {
+  return <div>
+      <div className="mb-3">
+       <Input value="" onChange={()=> null} placeholder="Rechercher..."/>
+       <Checkbox id="stocked" checked={false} onChange={()=> null} label="N'afficher que les produits en stock" /> 
+      </div>
+  </div>
+}
+
+function ProductTable ({products}) {
+  const rows = []
+  let lastCategory = null
+  for(let product of products){
+    if (product.category !== lastCategory){
+      rows.push(<ProductCategoryRow key={product.category} name={product.category} />)
+    }
+    lastCategory = product.category
+    rows.push(<ProductRow product={product} key={product.name} /> )
+  }
+  return <table className="table">
+            <thead>
+              <tr>
+                <th>Nom</th>
+                <th>Prix</th>
+              </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+            
+          </table>
+}
+
+export default App
